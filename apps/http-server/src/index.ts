427d10a549dc2d08a0c4484d987515571487128f
karan ahir <ahirkaran218@gmail.com>
2026-05-29 19:11:47 +0530

    add cors

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
-import cors from 'cors';
-import crypto from 'crypto';
 import { env } from '@repo/backend-common/config';
 import authRouter from './auth/routes.js';
-
+import cors from 'cors';
 const app = express();
 
 const PORT = env.PORT || 4000;
-
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Frontend URL
+    credentials: true, // Allow cookies/auth headers
+  })
+);
 app.use(express.json());
 
 app.use('/auth', authRouter);
